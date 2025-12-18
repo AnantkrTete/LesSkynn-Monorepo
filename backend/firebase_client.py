96e53663d0822bdf2c7424c20583e8ba1bf91ec5
AnantkrTete <135524288+AnantkrTete@users.noreply.github.com>
2025-12-18 11:16:32 +0530

    Update firebase_client.py

--- backend/firebase_client.py
+++ backend/firebase_client.py
@@ -1,8 +1,15 @@
+import os
+import json
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-cred = credentials.Certificate("firebase_service_account.json")
+# Prevent re-initialization in serverless
+if not firebase_admin._apps:
+    service_account_info = json.loads(
+        os.environ.get("FIREBASE_SERVICE_ACCOUNT")
+    )
 
-firebase_admin.initialize_app(cred)
+    cred = credentials.Certificate(service_account_info)
+    firebase_admin.initialize_app(cred)
 
 db = firestore.client()
