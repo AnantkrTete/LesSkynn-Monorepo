5ebd1e9bb785f6eaf2c648cdc2a02ac852d9f066
AnantkrTete <135524288+AnantkrTete@users.noreply.github.com>
2025-12-18 13:04:01 +0530

    Update firebase_client.py

--- backend/firebase_client.py
+++ backend/firebase_client.py
@@ -1,15 +1,12 @@
-import os, json
+import os
+import json
 from firebase_admin import credentials, firestore, initialize_app
 
 firebase_creds = os.getenv("FIREBASE_CREDENTIALS")
 if not firebase_creds:
     raise RuntimeError("FIREBASE_CREDENTIALS not set")
 
-creds = json.loads(firebase_creds)
-
-# THIS LINE IS REQUIRED
-creds["private_key"] = creds["private_key"].replace("\\n", "\n")
-
-cred = credentials.Certificate(creds)
+cred = credentials.Certificate(json.loads(firebase_creds))
 initialize_app(cred)
+
 db = firestore.client()
