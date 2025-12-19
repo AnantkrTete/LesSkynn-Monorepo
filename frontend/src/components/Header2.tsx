88e10d533050a4bc178c23a9be24bcea392ee2f1
AnantkrTete <youcanhackitbro@gmail.com>
2025-12-20 00:35:04 +0530

    fix font issue

--- frontend/src/components/Header2.tsx
+++ frontend/src/components/Header2.tsx
@@ -11,7 +11,7 @@ const Header = () => {
 const isBookingPage = location.pathname === "/booking";
 
   return (
-    <header className="fixed top-0 left-0 right-0 z-50 bg-white border-b border-black/40 backdrop-blur-sm">
+    <header className="fixed top-0 left-0 right-0 z-50 bg-white md:border-b border-black/40 backdrop-blur-sm">
       <div className="w-full px-4 sm:px-6 md:px-10 py-3 flex items-center justify-between">
 
         {/* LOGO/BRAND REMOVED */}
@@ -27,7 +27,9 @@ const isBookingPage = location.pathname === "/booking";
               alt="LesSkyn Logo"
               className="w-8 h-8 sm:w-10 sm:h-12 object-contain md:hidden"
             />
-
+            <span className="block md:hidden font-montserrat text-[13px] font-medium leading-none ">
+              LesSkyn
+            </span>
             {/* DESKTOP = show only when mode === 'dark' */}
             { (
               <img
